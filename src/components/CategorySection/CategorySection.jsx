d881dd3108958aac10b0fda54cae57f63766123f
mdarf <mdarfinji45@gmail.com>
2024-07-30 23:26:32 +0800

    emba carousel added

--- src/components/CategorySection/CategorySection.jsx
+++ src/components/CategorySection/CategorySection.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import { Button } from "../ui/button";
+import Autoplay from "embla-carousel-autoplay";
+
+export function Example() {
+  return (
+    <Carousel
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+    >
+      // ...
+    </Carousel>
+  );
+}
 
 const CategorySection = () => {
-  return <div>CategorySection</div>;
+  const category = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Data Science",
+    "Graphic Designer",
+    "FullStack Developer",
+  ];
+
+  return (
+    <div>
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+        className="w-full max-w-xl mx-auto my-20"
+      >
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+              <Button>{cat}</Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselNext></CarouselNext>
+        <CarouselPrevious></CarouselPrevious>
+      </Carousel>
+    </div>
+  );
 };
 
 export default CategorySection;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../shared/Navbar";
 import HeroSection from "../HeroSection/HeroSection";
+import CategorySection from "../CategorySection/CategorySection";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <HeroSection></HeroSection>
+      <CategorySection></CategorySection>
     </div>
   );
 };
