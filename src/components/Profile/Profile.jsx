463771e31bfe143737587fec105f044519fbb352
mdarf <mdarfinji45@gmail.com>
2024-08-09 00:00:39 +0800

    addinf profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Navbar from "../shared/Navbar";
+import { Avatar, AvatarImage } from "../ui/avatar";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      <div className="max-w-7xl mx-auto bg-white border-gray-200 rounded-2xl my-5 p-8">
+        <div className="flex items-center gap-4">
+          <div>
+            <Avatar className="h-24 w-24">
+              <AvatarImage src=""> </AvatarImage>
+            </Avatar>
+          </div>
+
+          <div>
+            <div>
+              <h1>Full Name</h1>
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquam, dignissimos.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
