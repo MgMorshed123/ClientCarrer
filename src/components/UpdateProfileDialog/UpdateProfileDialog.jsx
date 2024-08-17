29ab1398c5cc6a6fb2a2b2b739fe80d01597dfb8
mdarf <mdarfinji45@gmail.com>
2024-08-17 11:50:20 +0800

    added modal

--- src/components/UpdateProfileDialog/UpdateProfileDialog.jsx
+++ src/components/UpdateProfileDialog/UpdateProfileDialog.jsx
@@ -1,7 +1,97 @@
 import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "../ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const UpdateProfileDialog = ({ open, setOpen }) => {
-  return <div>UpdateProfileDialog</div>;
+  let loading;
+  return (
+    <div>
+      <Dialog open={open}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Update Profile</DialogTitle>
+          </DialogHeader>
+
+          <form onSubmit={""}>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  name="name"
+                  type="text"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="email" className="text-right">
+                  Email
+                </Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="number" className="text-right">
+                  Number
+                </Label>
+                <Input id="number" name="number" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="bio" className="text-right">
+                  Bio
+                </Label>
+                <Input id="bio" name="bio" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="skills" className="text-right">
+                  Skills
+                </Label>
+                <Input id="skills" name="skills" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="file" className="text-right">
+                  Resume
+                </Label>
+                <Input
+                  id="file"
+                  name="file"
+                  type="file"
+                  accept="application/pdf"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              {loading ? (
+                <Button className="w-full my-4">
+                  {" "}
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
+                </Button>
+              ) : (
+                <Button type="submit" className="w-full my-4">
+                  Update
+                </Button>
+              )}
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
 export default UpdateProfileDialog;
