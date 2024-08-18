8a2627a8e71dc0ba39675dee790596d31d9e844d
mdarf <mdarfinji45@gmail.com>
2024-08-18 10:17:03 +0800

    admin and companies

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./components/Redux/store.js";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "./components/ui/sonner.jsx";
 
+const persistor = persistStore(store);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
